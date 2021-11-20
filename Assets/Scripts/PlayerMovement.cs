3265371fd313607b0bcd48254385b994bb5eae43
vienhoanglong112 <vienhoanglong789@gmail.com>
2021-11-20 21:43:10 +0700

    Projectile

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,71 +4,83 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-  public ProjectileBehavior projectileBehavior;
-  public Transform LanchOffset;
-  private Rigidbody2D rb;
-  private Animator anim;
-  private SpriteRenderer sprite;
-  
-  [SerializeField] 
-  private float JumpForce = 1;
+    public ProjectileBehavior projectileBehavior;
+    public Transform LanchOffset;
+    private Rigidbody2D rb;
+    private Animator anim;
+    private SpriteRenderer sprite;
 
-  [SerializeField] 
-  private float MovementSpeed = 1;
-  private float inputX;
-  private enum MoveState{Idle,Run,Jump,Fall};
-  void Start()
-  {
-    rb = GetComponent<Rigidbody2D>();
-    anim = GetComponent<Animator>();
-    sprite = GetComponent<SpriteRenderer>();
-  }
+    [SerializeField]
+    private float JumpForce = 1;
 
-  // Update is called once per frame
-  void Update()
-  {
-    inputX = Input.GetAxisRaw("Horizontal");
-    rb.velocity = new Vector2(inputX * MovementSpeed, rb.velocity.y);
-    if (Input.GetAxisRaw("Vertical") > .1f && Mathf.Abs(rb.velocity.y) < 0.001f)
+    [SerializeField]
+    private float MovementSpeed = 1;
+    private float inputX;
+    private enum MoveState { Idle, Run, Jump, Fall };
+    void Start()
     {
-      //rb.AddForce(new Vector2(0, JumpForce), ForceMode2D.Impulse);
-      rb.velocity = new Vector2(rb.velocity.x, JumpForce);
+        rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+        sprite = GetComponent<SpriteRenderer>();
     }
 
-    UpdateAniamtionMovement();
-
-    if (Input.GetButtonDown("Jump"))
+    // Update is called once per frame
+    void Update()
     {
-      Debug.Log("Hit");
-      Instantiate(projectileBehavior, LanchOffset.position, transform.rotation);
-    }
-  }
-  // void FixedUpdate() {
-  //   transform.position += new Vector3(inputX,0,0) * Time.deltaTime * MovementSpeed;
-  // }
-  void UpdateAniamtionMovement(){
-    MoveState state;
-    if (inputX > 0f)
-    {
-      state = MoveState.Run;
-      sprite.flipX = false;
-    }
-    else if (inputX < 0f)
-    {
-      state = MoveState.Run;
-      sprite.flipX = true;
-    }
-    else {
-      state = MoveState.Idle;
-    }
+        inputX = Input.GetAxisRaw("Horizontal");
+        rb.velocity = new Vector2(inputX * MovementSpeed, rb.velocity.y);
 
-    if (rb.velocity.y > .1f){
-      state = MoveState.Jump;
-    }
-    else if (rb.velocity.y < -.1f){
-      state = MoveState.Fall;
-    }
+        if (Input.GetAxisRaw("Vertical") > .1f && Mathf.Abs(rb.velocity.y) < 0.001f)
+        {
+            //rb.AddForce(new Vector2(0, JumpForce), ForceMode2D.Impulse);
+            rb.velocity = new Vector2(rb.velocity.x, JumpForce);
+        }
 
-    anim.SetInteger("State",(int)state);
-  }
+        UpdateAniamtionMovement();
+        //
+        if (!Mathf.Approximately(0, inputX))
+        {
+            transform.rotation = inputX < 0 ? Quaternion.Euler(0, 180, 0) : Quaternion.identity;
+        }
+        //
+        if (Input.GetButtonDown("Jump"))
+        {
+            Debug.Log("Hit");
+            Instantiate(projectileBehavior, LanchOffset.position, transform.rotation);
+        }
+    }
+    // void FixedUpdate() {
+    //   transform.position += new Vector3(inputX,0,0) * Time.deltaTime * MovementSpeed;
+    // }
+    void UpdateAniamtionMovement()
+    {
+        MoveState state;
+        if (inputX > 0f)
+        {
+            state = MoveState.Run;
+            // sprite.flipX = false;
+            // transform.Rotate(0, 0.0f, 0.0f, Space.Self);
+        }
+        else if (inputX < 0f)
+        {
+            state = MoveState.Run;
+            // sprite.flipX = true;
+            // transform.Rotate(0, 180.0f, 0.0f, Space.Self);
+        }
+        else
+        {
+            state = MoveState.Idle;
+        }
+
+        if (rb.velocity.y > .1f)
+        {
+            state = MoveState.Jump;
+        }
+        else if (rb.velocity.y < -.1f)
+        {
+            state = MoveState.Fall;
+        }
+
+        anim.SetInteger("State", (int)state);
+    }
 }
